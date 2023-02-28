78ffd0f9ef0e87aa2b23ec5f71cf5647f5c348ba
m.sobikanth <m.sobikanth@msobikanths-macbook-pro.local>
2023-02-28 09:43:59 +0530

    Third Commit

--- MyFirstProject/src/Main_1.java
+++ MyFirstProject/src/Main_1.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class Main_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String name;
-		name = "Hello";
+
+		// GUI Intro
+
+		/*
+		 * String name = JOptionPane.showInputDialog("Enter Your Name");
+		 * JOptionPane.showMessageDialog(null, "Hello " + name);
+		 */
+
+		// Math Class
+
+		/*
+		 * double x, y, z;
+		 * 
+		 * Scanner scanner = new Scanner(System.in);
+		 * 
+		 * System.out.println("Enter side x"); x=scanner.nextDouble();
+		 * System.out.println("Enter side y"); y=scanner.nextDouble();
+		 * 
+		 * z=Math.sqrt((x*x)+(y*y));
+		 * 
+		 * System.out.println("Z is "+z);
+		 * 
+		 * scanner.close();
+		 */
+
+		// Random numbers
+		Random random = new Random();
+		int x = random.nextInt();
+		System.out.println(x);
 
 	}
 
